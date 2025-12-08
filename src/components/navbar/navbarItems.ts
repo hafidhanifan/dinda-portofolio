3e501d4eb558f4385e9b974fb0059fe87c65d52d
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-12-08 13:52:55 +0700

    modified:   src/components/navbar/navbarItems.ts

--- src/components/navbar/navbarItems.ts
+++ src/components/navbar/navbarItems.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { id: "hero", label: "Home" },
-  { id: "portfolio", label: "Portfolio" },
-  { id: "about", label: "About Me" },
+  { id: "portfolio", label: "Profile" },
+  { id: "about", label: "Portofolio" },
   { id: "contact", label: "Contact" },
 ];
