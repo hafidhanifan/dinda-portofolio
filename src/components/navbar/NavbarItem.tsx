aff4909cb07050d587a8d738ef2e38b6c9f09e1b
Hafid Hanifan <hafid.hanifan123@gmail.com>
2026-01-06 20:18:50 +0700

    modified:   src/components/navbar/NavbarItem.tsx

--- src/components/navbar/NavbarItem.tsx
+++ src/components/navbar/NavbarItem.tsx
@@ -1,24 +1,40 @@
 import React from "react";
+import { motion, scale } from "motion";
 
 interface NavbarItemProps {
   label: string;
+  icon: React.ComponentType<{ size?: number }>; // tipe untuk ion
   isActive: boolean;
+  isHovered: boolean; // untuk tau apakah item ini sedang di-hover
   onClick: () => void;
+  onHoverStart: () => void; //fungsi saat mulai hover
+  onHoverEnd: () => void; // fungsi saat selesai hover
 }
 
 const NavbarItem: React.FC<NavbarItemProps> = ({
   label,
+  icon: Icon, // menggunakan huruf besar supaya bisa dipake sebagai component
   isActive,
+  isHovered,
   onClick,
+  onHoverStart,
+  onHoverEnd,
 }) => {
   return (
-    <button
-      onClick={onClick}
-      className={`flex flex-col items-center text-xs transition 
-        ${isActive ? "text-black font-semibold" : "text-gray-500"}`}
-    >
-      {label}
-    </button>
+    <motion.div
+      className="relative flex flex-col items-center"
+      // event hover - panggil fungsi dari parent
+      onHoverStart={onHoverStart}
+      onHoverEnd={onHoverEnd}
+      // aniamsi jarak menjauh ketika di-hover
+      animate={{
+        scale: isHovered ? 1.05 : 1, // digunakan untuk memperbesar saat hover
+      }}
+      transition={{
+        duration: 0.3, // durasi animasi 0.3 detik
+        ease: "easeInOut", // jenis easing (percepatan animasi)
+      }}
+    ></motion.div>
   );
 };
 
