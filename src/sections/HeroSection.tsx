5b59f2b617993bb08e1164c5d21edab42cfcfd46
Hafid Hanifan <hafid.hanifan123@gmail.com>
2026-01-29 20:58:34 +0700

    new file:   src/assets/backgrounds/hero_image.jpg 	modified:   src/sections/HeroSection.tsx

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,10 +1,16 @@
+import heroImage from "../assets/backgrounds/hero_image.jpg";
+
 export default function HeroSection() {
   return (
     <section
       id="hero"
       className="min-h-screen flex items-center justify-center"
     >
-      <h1>This is hero section</h1>
+      <img
+        src={heroImage}
+        alt="Dinda Background"
+        className="absolute inset-0 w-full object-cover"
+      />
     </section>
   );
 }
