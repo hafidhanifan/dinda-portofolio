0a86153ac43a511fa86b5dd58d9ffc9dba2bca39
Hafid Hanifan <hafid.hanifan123@gmail.com>
2026-02-02 19:55:46 +0700

    modified:   src/sections/AboutSection.tsx

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -2,7 +2,7 @@ import TextReveal from "../components/animations/TextReveal";
 
 const AboutSection = () => {
   return (
-    <section className="w-full">
+    <section className="w-full mb-96">
       <TextReveal text="Hello, my name is Dinda Farrass A. I'm a tech enthusiast studying Informatics Engineering, passionate about creating intuitive and visually appealing experiences." />
     </section>
   );
