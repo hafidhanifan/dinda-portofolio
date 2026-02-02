01e135b45c13dbeccc0b72dea9f649ae73b09c2b
Hafid Hanifan <hafid.hanifan123@gmail.com>
2026-02-02 19:12:24 +0700

    new file:   src/sections/AboutSection.tsx

---
+++ src/sections/AboutSection.tsx
@@ -0,0 +1,11 @@
+import TextReveal from "../components/animations/TextReveal";
+
+const AboutSection = () => {
+  return (
+    <section className="w-full">
+      <TextReveal text="Hello, my name is Dinda Farrass A. I'm a tech enthusiast studying Informatics Engineering, passionate about creating intuitive and visually appealing experiences." />
+    </section>
+  );
+};
+
+export default AboutSection;
